b2d42de7bd496eaed7d99da2724e853f6b00d0ed
Nikolay Kudyn <nikolay.kudyn@gmail.com>
2018-03-21 00:10:55 +0200

    Improvement of font-awesome importing

--- troleybus-schedule-client/src/main.js
+++ troleybus-schedule-client/src/main.js
@@ -4,25 +4,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import '@fortawesome/fontawesome'
+import '@fortawesome/fontawesome-free-solid'
+import '@fortawesome/fontawesome-free-brands'
+import '@fortawesome/fontawesome-free-regular'
+
 import ScheduleTable from '@/components/ScheduleTable'
 import ScheduleButtons from '@/components/ScheduleButtons'
-import fontawesome from '@fortawesome/fontawesome'
-import brands from '@fortawesome/fontawesome-free-brands'
-import { faCoffee } from '@fortawesome/fontawesome-free-solid'
 
-Vue.use(BootstrapVue);
-Vue.config.productionTip = false;
-Vue.component('schedule-table', ScheduleTable);
-Vue.component('buttons-service', ScheduleButtons);
-
-fontawesome.library.add(brands, faCoffee)
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+Vue.component('schedule-table', ScheduleTable)
+Vue.component('schedule-buttons', ScheduleButtons)
 
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-});
+})
