0932f3ee46267957ec7b61dee83233b9bb97278b
DanillGodovan <bonkinans@gmail.com>
2018-03-22 16:59:10 +0200

    Deleted special symbols

--- troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
+++ troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="container-fluid">
-      <div class="first"><h5><input name="number" type="radio" value="number"> 2018-03-17 &nbsp;&nbsp;&nbsp;&nbsp;<font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_20_02_2018.xlsx             <b-badge pill variant="secondary">15</b-badge> маршрутів  <b-badge variant="primary">Чорновий</b-badge>  <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/></a>    <a href="#"><font-awesome-icon :icon="['fas', 'save']" class="text-success"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b-button variant="primary">Імпортувати файл</b-button></a></h5></div>
+      <h5><input name="number" type="radio" value="number"> 2018-03-17      <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_20_02_2018.xlsx             <b-badge pill variant="secondary">15</b-badge> маршрутів  <b-badge variant="primary">Чорновий</b-badge>  <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/></a>    <a href="#"><font-awesome-icon :icon="['fas', 'save']" class="text-success"/>         <b-button variant="primary">Імпортувати файл</b-button></a></h5>
 
-      <div class="second"><h5><input name="number" type="radio" value="number"> 2018-03-11  &nbsp;&nbsp;&nbsp;&nbsp;<font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_02_2018.xlsx                <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="success">Діючий</b-badge>        <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /></a>   <span style='padding-left:10px;'><input type="text" size="40"></span></h5></div>
+      <h5><input name="number" type="radio" value="number"> 2018-03-11      <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_02_2018.xlsx                <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="success">Діючий</b-badge>        <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /></a>   <input type="text" size="40"></h5>
 
-      <div class="third"><h5><input name="number" type="radio" value="number" > 2018-02-10 &nbsp;&nbsp;&nbsp;&nbsp;<font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_01_2018.xlsx            <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="secondary">Минулий</b-badge>   <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /> </a></h5></div>
+      <h5><input name="number" type="radio" value="number" > 2018-02-10 <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_01_2018.xlsx            <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="secondary">Минулий</b-badge>   <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /> </a></h5>
     </div>
   </div>
 </template>
