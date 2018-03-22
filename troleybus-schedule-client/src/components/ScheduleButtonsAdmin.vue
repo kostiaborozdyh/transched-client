41a25c6d9f63171e7016a452ad3cee8eb2035f3d
DanillGodovan <bonkinans@gmail.com>
2018-03-22 18:06:51 +0200

    Removed spaces

--- troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
+++ troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
     <div class="container-fluid">
-      <h5><input name="number" type="radio" value="number"> 2018-03-17      <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_20_02_2018.xlsx             <b-badge pill variant="secondary">15</b-badge> маршрутів  <b-badge variant="primary">Чорновий</b-badge>  <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/></a>    <a href="#"><font-awesome-icon :icon="['fas', 'save']" class="text-success"/>         <b-button variant="primary">Імпортувати файл</b-button></a></h5>
-
-      <h5><input name="number" type="radio" value="number"> 2018-03-11      <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_02_2018.xlsx                <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="success">Діючий</b-badge>        <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /></a>   <input type="text" size="40"></h5>
-
-      <h5><input name="number" type="radio" value="number" > 2018-02-10 <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_01_2018.xlsx            <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="secondary">Минулий</b-badge>   <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /> </a></h5>
+      <h5><input name="number" type="radio" value="number"> 2018-03-17<font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_20_02_2018.xlsx<b-badge pill variant="secondary">15</b-badge> маршрутів  <b-badge variant="primary">Чорновий</b-badge>  <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/></a><a href="#"><font-awesome-icon :icon="['fas', 'save']" class="text-success"/><b-button variant="primary">Імпортувати файл</b-button></a></h5>
+      <h5><input name="number" type="radio" value="number"> 2018-03-11<font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_02_2018.xlsx<b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="success">Діючий</b-badge>        <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /></a><input type="text" size="40"></h5>
+      <h5><input name="number" type="radio" value="number" > 2018-02-10 <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_01_2018.xlsx<b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="secondary">Минулий</b-badge>   <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /> </a></h5>
     </div>
   </div>
 </template>
