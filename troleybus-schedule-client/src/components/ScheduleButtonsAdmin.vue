71c37d743ae7a2bee840401dd435b3af2a521667
DanillGodovan <bonkinans@gmail.com>
2018-03-24 22:48:44 +0200

    Fixed small errors

--- troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
+++ troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
-    <div class="container-fluid">
-      <div class="col col-sm-12 col-m-12 col-lg-12"><h5><input name="number" type="radio" value="number"> 2018-03-17  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_20_02_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів  <b-badge variant="primary">Чорновий</b-badge>  <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/></a>   <a href="#"><font-awesome-icon :icon="['fas', 'save']" class="text-success"/></a>     </h5></div>
+    <b-container fluid>
+      <div class="col"><h5><input name="number" type="radio" value="number"> 2018-03-17  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_20_02_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів  <b-badge variant="primary">Чорновий</b-badge>  <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/></a>   <a href="#"><font-awesome-icon :icon="['fas', 'save']" class="text-success"/></a>     </h5></div>
       <div class="button">
         <b-button variant="primary">Імпортувати файл</b-button>
       </div>
-      <div class="col col-sm-12 col-m-12 col-lg-12"><h5><input name="number" type="radio" value="number"> 2018-03-11  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_02_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="success">Діючий</b-badge>    <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /></a>    </h5></div>
-      <div class="col-md-12">
+      <div class="col"><h5><input name="number" type="radio" value="number"> 2018-03-11  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_02_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="success">Діючий</b-badge>    <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /></a>    </h5></div>
       <div class="input">
         <b-form-input id="input-default" type="text" placeholder="C:/Documents and Settings"></b-form-input>
       </div>
-        </div>
-      <div class="col col-sm-12 col-m-12 col-lg-12"><h5><input name="number" type="radio" value="number" > 2018-02-10  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_01_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="secondary">Минулий</b-badge>     <a href="#">  <font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /> </a></h5></div>
-  </div>
+      <div class="col"><h5><input name="number" type="radio" value="number" > 2018-02-10  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_01_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="secondary">Минулий</b-badge>     <a href="#">  <font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /> </a></h5></div>
+  </b-container>
   </div>
 </template>
 <script>
@@ -29,10 +27,10 @@
     margin-left: 20px;
   }
   .button{
-    margin-left: 920px;
+    margin-left: 880px;
   }
   .input{
     margin-left: 840px;
-    margin-right: 280px;
+    margin-right: 520px;
   }
 </style>
