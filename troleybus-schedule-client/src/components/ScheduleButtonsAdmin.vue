84208418a54ed0a2039b663648cbef27a5b018f4
DanillGodovan <bonkinans@gmail.com>
2018-03-25 15:15:39 +0300

    Bootstra + vue input has deleted

--- troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
+++ troleybus-schedule-client/src/components/ScheduleButtonsAdmin.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="col"><h5><input name="number" type="radio" value="number"> 2018-03-11  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_02_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="success">Діючий</b-badge>    <a href="#"><font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /></a>    </h5></div>
       <div class="input">
-        <b-form-input id="input-default" type="text" placeholder="C:/Documents and Settings"></b-form-input>
+        <input type="text" size="30" maxlength="10">
       </div>
       <div class="col"><h5><input name="number" type="radio" value="number" > 2018-02-10  <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/> Файл_розкладу_10_01_2018.xlsx  <b-badge pill variant="secondary">15</b-badge> маршрутів <b-badge variant="secondary">Минулий</b-badge>     <a href="#">  <font-awesome-icon :icon="['fas', 'trash']" class="text-danger" /> </a></h5></div>
   </b-container>
