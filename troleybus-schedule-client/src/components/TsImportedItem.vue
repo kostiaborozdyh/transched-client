b3ed141e860d7fa22c41596e7addc7f5e18e0caf
Nikolay Kudyn <nikolay.kudyn@gmail.com>
2018-04-01 01:34:48 +0300

    Fix of incorrect comparison

--- troleybus-schedule-client/src/components/TsImportedItem.vue
+++ troleybus-schedule-client/src/components/TsImportedItem.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="row">
-      <div class="col-2">{{item.version}}</div>
-      <div class="col-4">
-        <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/>
-        <span>{{item.filename}}</span>
-      </div>
-      <div class="col-3">
-        <span><b-badge pill variant="secondary">{{item.routes_count}}</b-badge></span>
-        <span>маршрутів</span>
-      </div>
-      <div class="col-3 text-right">
-        <b-badge v-bind:variant="">{{item.state}}</b-badge>
-        <a href="#">
-          <font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/>
-        </a>
-        <a href="#" v-show="item.state == 'draft'">
-          <font-awesome-icon :icon="['fas', 'save']" class="text-success"/>
-        </a>
-      </div>
+    <div class="col-2">{{item.version}}</div>
+    <div class="col-4">
+      <font-awesome-icon :icon="['fas', 'file-excel']" class="text-success"/>
+      <span>{{item.filename}}</span>
     </div>
+    <div class="col-3">
+      <span><b-badge pill variant="secondary">{{item.routes_count}}</b-badge></span>
+      <span>маршрутів</span>
+    </div>
+    <div class="col-3 text-right">
+      <b-badge v-bind:variant="">{{item.state}}</b-badge>
+      <a href="#">
+        <font-awesome-icon :icon="['fas', 'trash']" class="text-danger"/>
+      </a>
+      <a href="#" v-show="item.state == 'draft'">
+        <font-awesome-icon :icon="['fas', 'save']" class="text-success"/>
+      </a>
+    </div>
+  </div>
 </template>
 <script>
   import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
   export default {
     props: ['item'],
     components: {
@@ -29,15 +30,15 @@
     },
     computed: {
       statusBadgeVariantType: function () {
-        var budgeType;
-        if (this.item.state = 'draft') {
+        let budgeType
+        if (this.item.state === 'draft') {
           budgeType = 'primary'
-        } else if (this.item.state = 'active') {
+        } else if (this.item.state === 'active') {
           budgeType = 'success'
         } else {
           budgeType = 'secondary'
         }
-        return budgeType;
+        return budgeType
       }
     }
   }
