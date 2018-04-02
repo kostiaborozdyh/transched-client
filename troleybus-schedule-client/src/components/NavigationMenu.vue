3843bc77ab05618f81d4b9365e50400803a5f879
DanillGodovan <bonkinans@gmail.com>
2018-04-02 17:02:35 +0300

    added icons for Navigation menu

--- troleybus-schedule-client/src/components/NavigationMenu.vue
+++ troleybus-schedule-client/src/components/NavigationMenu.vue
@@ -1,13 +1,18 @@
 <template>
   <b-nav pills>
-    <b-nav-item to="schedule">Розклад руху</b-nav-item>
-    <b-nav-item to="stops">Редагування зупиннок</b-nav-item>
+    <b-nav-item to="schedule"><font-awesome-icon :icon="['far', 'calendar']" /> Розклад руху</b-nav-item>
+    <b-nav-item to="stops"><font-awesome-icon :icon="['fas', 'map-marker']" "/> Редагування зупиннок</b-nav-item>
   </b-nav>
 </template>
 
 <script>
+  import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
   export default {
-    name: 'NavigationMenu'
+    name: 'NavigationMenu',
+    components: {
+      FontAwesomeIcon
+    }
   }
 </script>
 
